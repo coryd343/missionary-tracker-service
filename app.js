7ce2d28d5ff82f193acf8e28a357767891c7a832
Cory <fizzy.gutsnipe@outlook.com>
2019-02-23 01:35:14 -0800

    removed front end files

--- app.js
+++ app.js
@@ -1,45 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  for (i = 1; i < 7; i++) {
+      //write a response to the client
+      res.write('<h' + i + ' style="color:blue">Missionary-Tracker Back End</h' + i + '>'); 
+  }
+  res.end(); //end the response
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+let index = require('./routes/index.js');
+app.use('/index', index);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+let users = require('./routes/users.js');
+app.use('/users', users);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server up and running on port: " + (process.env.PORT || 3000));
 });
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server up and running on port: " + (process.env.PORT || 5000));
-});
-
-module.exports = app;
