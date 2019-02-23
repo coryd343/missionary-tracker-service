efbe800866f639a7d467fdda94ad7e1ef0fcd8c6
Cory <fizzy.gutsnipe@outlook.com>
2019-02-23 13:03:32 -0800

    db connection successful

--- utils/sql_conn.js
+++ utils/sql_conn.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 pgp.pg.defaults.ssl = true;
 
-const db = pgp('postgres://mbsvmpwnwzkdam:c49e634ab69c111d3a2402a32bc944a28437d0e6aa0fabf3569cc4e7655680cd@ec2-50-17-193-83.compute-1.amazonaws.com:5432/d5mijhl06l39lt');
+const db = pgp('postgres://lapabxcqafqmfn:a7d0d096e991558d05662851b6b82cbc3de4366beb5f173214427f63abf70832@ec2-54-83-17-151.compute-1.amazonaws.com:5432/d10e2q6lc36qms');
 
 module.exports = db;
